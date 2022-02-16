1aa35ebfe237260393c98ffbfc5175db71f432de
crow <corvusmacrorhynchosjaponensis@gmail.com>
2022-02-16 18:55:39 +0900

    modmenu-3.0.1.jarの追加とビルド

---
+++ src/main/java/com/Hashibutogarasu/commandkeybind/CommandKeyBindModClient.java
@@ -0,0 +1,13 @@
+package com.Hashibutogarasu.commandkeybind;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CommandKeyBindModClient implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
